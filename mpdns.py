e02e22bd79e6416f93d9322f374f7c3f948209b5
nopernik <nopernik@gmail.com>
2021-07-24 16:25:11 +0300

    adding HTTPS query type 65

--- mpdns.py
+++ mpdns.py
@@ -70,7 +70,7 @@ qTypeDict = {1:'A', 2:'NS', 5:'CNAME', 6:'SOA', 12:'PTR', 15:'MX',
             45:'IPSECKEY', 46:'RRSIG', 47:'NSEC', 48:'DNSKEY', 49:'DHCID',
             50:'NSEC3', 51:'NSEC3PARAM', 52:'TLSA', 55:'HIP', 99:'SPF',
             249:'TKEY', 250:'TSIG', 251:'IXFR', 252:'AXFR', 255:'ANY',
-            257:'CAA', 32768:'TA', 32769:'DLV'}
+            257:'CAA', 32768:'TA', 32769:'DLV', 65:'HTTPS'}
 
 def parseDBFile(hostFile=hostFile):
     with open(hostFile,'r') as f:
